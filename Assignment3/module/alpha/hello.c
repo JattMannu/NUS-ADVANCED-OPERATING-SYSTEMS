83f67dd58df16a224566fb7ea0040a95b958d23f
JattMannu <manpreet_firewall@yahoo.com.sg>
2019-03-17 01:53:48 +0800

    Added Task 1

--- Assignment3/module/alpha/hello.c
+++ Assignment3/module/alpha/hello.c
@@ -1,18 +1,15 @@
+#include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
 MODULE_LICENSE("GPL");
-//Init Code
-static int hello_init(void){
-    //Using Linux Kernal's Print
-    printk(KERN_ALERT "Hello, Manpreet!\n");
-    return 0;
+static int hello_init(void)
+{
+printk(KERN_ALERT "Hello, world\n");
+return 0;
 }
-
-static void hello_exit(void){
-    printk(KERN_ALERT "Good bye!\n");
+static void hello_exit(void)
+{
+printk(KERN_ALERT "Goodbye, cruel world\n");
 }
-
-//When you start this module jump to hello_init
 module_init(hello_init);
-//When you stop this module jump to hello_exit
 module_exit(hello_exit);
